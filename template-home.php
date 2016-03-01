6c2ca2de3634cc29e394ebb0c676c67473a84cc3
Jimmy Zooler <jimmy.zooler.com>
2016-03-01 16:56:05 -0500

    Styled hero, added columns to home

--- template-home.php
+++ template-home.php
@@ -11,9 +11,21 @@ Template Name: Home Page
             <div class="small-centered medium-uncentered large-12 medium-12 columns">
                 
                 <h1 class="hero-heading"><?php the_field('heading_'); ?></h1>
+                <h3 class="hero-heading"><?php the_field('heading_2'); ?></h3>
               
             </div>
         </div>
+    </section>
+    <section class="second-home">
+        <div class="row">
+            <div class="left-column medium-4 columns">
+                <?php the_field('column_1_content'); ?>
+            </div>
+            <div class="right-column medium-8 columns">
+                <?php the_field('column_2_content'); ?>
+            </div>
+        </div>
+
     </section>
 
 <?php get_footer(); ?>
